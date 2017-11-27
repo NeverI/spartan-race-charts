f91abadb71c0bc0c0beb305d4e5ec48185c5655c
Kiss Gabor <nevi@agmen.hu>
2017-11-27 08:10:06 +0100

    2017 CEU races added

---
+++ app/races.js
@@ -0,0 +1,209 @@
+races = [
+  {
+    date: '2017-10-22',
+    country: 'HUN',
+    city: 'Eplény',
+    type: 'Sprint',
+    url: 'https://drive.google.com/open?id=10JZCTfEEVTn_xsJxrz4Tq6tT9UYeoJfg',
+    map: 'https://drive.google.com/file/d/0Bx_qmqQOXLBVV0FNZzhadUp2Zmc/view',
+    video: 'https://www.facebook.com/SpartanRaceHungary/videos/1737858706286459/'
+  },
+  {
+    date: '2017-10-21',
+    country: 'HUN',
+    city: 'Eplény',
+    type: 'Beast',
+    url: 'https://drive.google.com/file/d/1fG6QIkSNdX2BczkOq1fwSTsMCr5AMrjT',
+    map: 'https://drive.google.com/file/d/0Bx_qmqQOXLBVbnNQZ0MyblRXZnM/view',
+    video: 'https://www.facebook.com/SpartanRaceHungary/videos/1736909223048074/'
+  },
+  {
+    date: '2017-09-24',
+    country: 'CZE',
+    city: 'Liberec',
+    type: 'Sprint',
+    url: 'http://results.onlinesystem.cz/ResultsDetail/ExportResult/?eventId=837&trackId=1438',
+    map: '',
+    video: ''
+  },
+  {
+    date: '2017-09-23',
+    country: 'CZE',
+    city: 'Liberec',
+    type: 'Sprint',
+    url: 'http://results.onlinesystem.cz/ResultsDetail/ExportResult/?eventId=836&trackId=1433',
+    map: '',
+    video: ''
+  },
+  {
+    date: '2017-09-09',
+    country: 'SVK',
+    city: 'Kubínská Hola',
+    type: 'Sprint',
+    url: 'http://results.onlinesystem.cz/ResultsDetail/ExportResult/?eventId=840&trackId=1365',
+    map: '',
+    video: ''
+  },
+  {
+    date: '2017-09-09',
+    country: 'SVK',
+    city: 'Kubínská Hola',
+    type: 'Beast',
+    url: 'http://results.onlinesystem.cz/ResultsDetail/ExportResult/?eventId=840&trackId=1366',
+    map: '',
+    video: ''
+  },
+  {
+    date: '2017-08-26',
+    country: 'POL',
+    city: 'Warsaw',
+    type: 'Sprint',
+    url: 'http://results.onlinesystem.cz/ResultsDetail/ExportResult/?eventId=825&trackId=1303',
+    map: '',
+    video: ''
+  },
+  {
+    date: '2017-08-19',
+    country: 'CZE',
+    city: 'Litovel',
+    type: 'Sprint',
+    url: 'http://results.onlinesystem.cz/ResultsDetail/ExportResult/?eventId=833&trackId=1257',
+    map: '',
+    video: ''
+  },
+  {
+    date: '2017-07-30',
+    country: 'POL',
+    city: 'Krynica',
+    type: 'Sprint',
+    url: 'http://results.onlinesystem.cz/ResultsDetail/ExportResult/?eventId=890&trackId=1255',
+    map: '',
+    video: ''
+  },
+  {
+    date: '2017-07-30',
+    country: 'POL',
+    city: 'Krynica',
+    type: 'Super',
+    url: 'http://results.onlinesystem.cz/ResultsDetail/ExportResult/?eventId=890&trackId=1256',
+    map: '',
+    video: ''
+  },
+  {
+    date: '2017-07-16',
+    country: 'SVK',
+    city: 'Valčianska dolina',
+    type: 'Super',
+    url: 'http://results.onlinesystem.cz/ResultsDetail/ExportResult/?eventId=830&trackId=1239',
+    map: '',
+    video: ''
+  },
+  {
+    date: '2017-07-15',
+    country: 'SVK',
+    city: 'Valčianska dolina',
+    type: 'Sprint',
+    url: 'http://results.onlinesystem.cz/ResultsDetail/ExportResult/?eventId=829&trackId=1229',
+    map: '',
+    video: ''
+  },
+  {
+    date: '2017-07-15',
+    country: 'SVK',
+    city: 'Valčianska dolina',
+    type: 'Ultra Beast',
+    url: 'http://results.onlinesystem.cz/ResultsDetail/ExportResult/?eventId=829&trackId=1230',
+    map: '',
+    video: ''
+  },
+  {
+    date: '2017-07-01',
+    country: 'POL',
+    city: 'Trójmiasto',
+    type: 'Sprint',
+    url: 'http://results.onlinesystem.cz/ResultsDetail/ExportResult/?eventId=834&trackId=1220',
+    map: '',
+    video: ''
+  },
+  {
+    date: '2017-06-17',
+    country: 'HUN',
+    city: 'Kazincbarcika',
+    type: 'Super',
+    url: 'http://results.onlinesystem.cz/ResultsDetail/ExportResult/?eventId=827&trackId=1209',
+    map: '',
+    video: ''
+  },
+  {
+    date: '2017-05-20',
+    country: 'POL',
+    city: 'Lodz',
+    type: 'Sprint',
+    url: 'http://results.onlinesystem.cz/ResultsDetail/ExportResult/?eventId=831&trackId=1324',
+    map: '',
+    video: ''
+  },
+  {
+    date: '2017-05-13',
+    country: 'SVK',
+    city: 'Nitra',
+    type: 'Sprint',
+    url: 'http://results.onlinesystem.cz/ResultsDetail/ExportResult/?eventId=869&trackId=1125',
+    map: '',
+    video: ''
+  },
+  {
+    date: '2017-04-30',
+    country: 'CZE',
+    city: 'Kouty',
+    type: 'Sprint',
+    url: 'http://results.onlinesystem.cz/ResultsDetail/ExportResult/?eventId=823&trackId=1104',
+    map: '',
+    video: ''
+  },
+  {
+    date: '2017-04-29',
+    country: 'CZE',
+    city: 'Kouty',
+    type: 'Super',
+    url: 'http://results.onlinesystem.cz/ResultsDetail/ExportResult/?eventId=822&trackId=1103',
+    map: '',
+    video: ''
+  },
+  {
+    date: '2017-04-08',
+    country: 'POL',
+    city: 'Krakow',
+    type: 'Sprint',
+    url: 'http://results.onlinesystem.cz/ResultsDetail/ExportResult/?eventId=556&trackId=1245',
+    map: '',
+    video: ''
+  },
+  {
+    date: '2017-04-01',
+    country: 'HUN',
+    city: 'Veszprém',
+    type: 'Sprint',
+    url: 'http://results.onlinesystem.cz/ResultsDetail/ExportResult/?eventId=821&trackId=967',
+    map: '',
+    video: ''
+  },
+  {
+    date: '2017-02-18',
+    country: 'SVK',
+    city: 'Svit',
+    type: 'Winter Sprint',
+    url: 'http://results.onlinesystem.cz/ResultsDetail/ExportResult/?eventId=790&trackId=1357',
+    map: '',
+    video: ''
+  },
+  {
+    date: '2017-01-14',
+    country: 'CZE',
+    city: 'Dolni Morava',
+    type: 'Winter Sprint',
+    url: 'http://results.onlinesystem.cz/ResultsDetail/ExportResult/?eventId=789&trackId=1325',
+    map: '',
+    video: ''
+  }
+];
